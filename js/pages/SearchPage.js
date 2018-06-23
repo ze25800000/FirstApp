cd2dfdd520c9e9b975588a381947560205e33ecc
ze25800000 <ze258100000@sina.com>
2018-06-23 19:36:13 +0800

    9-2 带搜索的NavBar实现-2

--- js/pages/SearchPage.js
+++ js/pages/SearchPage.js
@@ -7,27 +7,74 @@ import {
     ListView,
     RefreshControl,
     DeviceEventEmitter,
+    TouchableOpacity,
     Platform,
     TextInput
 } from 'react-native'
 import NavigationBar from '../common/NavigationBar'
 import ViewUtils from '../common/util/ViewUtils'
+import GlobalStyles from '../../res/styles/GlobalStyles'
 
 export default class PopularPage extends Component {
-    onBackPress() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            rightButtonText: '搜索'
+        }
+    }
 
+    updateState(dic) {
+        this.setState(dic)
+    }
+
+    onBackPress() {
+        this.refs.input.blur()
+        this.props.navigator.pop()
+    }
+
+    onRightButtonClick() {
+        if (this.state.rightButtonText === '搜索') {
+            this.updateState({
+                rightButtonText: '取消'
+            })
+        } else {
+            this.updateState({
+                rightButtonText: '搜索'
+            })
+        }
     }
 
     renderNavBar() {
         let backButton = ViewUtils.getLeftButton(() => this.onBackPress())
         let inputView = <TextInput
+            ref={'input'}
+            underlineColorAndroid={'white'}
             style={styles.textInput}
         >
 
         </TextInput>
-        return <View>
+        let rightButton =
+            <TouchableOpacity
+                onPress={() => {
+                    this.onRightButtonClick()
+                    this.refs.input.blur()
+                }}
+            >
+                <View style={{marginRight: 10}}>
+                    <Text style={styles.title}>{this.state.rightButtonText}</Text>
+                </View>
+            </TouchableOpacity>
+        return <View
+            style={{
+                backgroundColor: '#2196f3',
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: (Platform.OS === 'ios') ? GlobalStyles.nav_bar_height_ios : GlobalStyles.nav_bar_height_android
+            }}
+        >
             {backButton}
             {inputView}
+            {rightButton}
         </View>
     }
 
@@ -38,7 +85,7 @@ export default class PopularPage extends Component {
                 style={[styles.statusBar, {backgroundColor: '#2196F3'}]}
             />
         }
-        return <View>
+        return <View style={GlobalStyles.root_container}>
             {statusBar}
             {this.renderNavBar()}
         </View>
@@ -54,8 +101,20 @@ const styles = StyleSheet.create({
     },
     textInput: {
         flex: 1,
-        height: (Platform.OS === 'ios') ? 30 : 30,
-        borderWidth: 1,
-        borderColor: 'white'
+        height: (Platform.OS === 'ios') ? 30 : 40,
+        borderWidth: (Platform.OS === 'ios') ? 0.4 : 0,
+        borderColor: 'white',
+        alignSelf: 'center',
+        paddingLeft: 5,
+        marginRight: 10,
+        marginLeft: 5,
+        borderRadius: 3,
+        opacity: 0.7,
+        color: 'white'
+    },
+    title: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: '500'
     }
 })
--- res/styles/GlobalStyles.js
+++ res/styles/GlobalStyles.js
@@ -8,5 +8,7 @@ module.exports = {
     root_container: {
         flex: 1,
         backgroundColor: '#f3f3f4'
-    }
+    },
+    nav_bar_height_ios: 40,
+    nav_bar_height_android: 50
 }
